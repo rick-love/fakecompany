0b8abc61eac64e58b8ad5313e8ebc4d714e56672
rick-love <ricklove@outlook.com>
2017-07-16 21:24:57 -0400

    adding files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+setTimeout(function(){
+	document.getElementById('modal').style.display = 'block' }, 3000);
+
+closeModal.addEventListener("click", function(){
+	document.getElementById('modal').style.display = 'none';
+});
+
